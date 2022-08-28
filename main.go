60613b5d79b59c56489f38e2e1c8f69fcbb9f0ac
Danila Egorov <c4nsero@mail.ru>
2022-08-28 18:33:43 +0300

    update main: initial commit

---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"tgBot/mods"
+
+	"github.com/spf13/viper"
+)
+
+func main() {
+	// https://core.telegram.org/bots/api#video
+
+	// Инициализация конфига (токенов)
+	err := mods.InitConfig()
+	if err != nil {
+		fmt.Println("Config error: ", err)
+		return
+	}
+
+	// Url бота для отправки и приёма сообщений
+	botUrl := "https://api.telegram.org/bot" + viper.GetString("token")
+	offSet := 0
+
+	// Цикл работы приложения
+	for {
+
+		// Получение апдейтов
+		updates, err := getUpdates(botUrl, offSet)
+		if err != nil {
+			fmt.Println("Something went wrong: ", err)
+		}
+
+		// Обработка апдейтов
+		for _, update := range updates {
+			respond(botUrl, update)
+			offSet = update.UpdateId + 1
+		}
+
+		// Вывод в консоль для тестов
+		fmt.Println(updates)
+	}
+}
+
+// Функция получения апдейтов
+func getUpdates(botUrl string, offset int) ([]mods.Update, error) {
+
+	// Rest запрос для получения апдейтов
+	resp, err := http.Get(botUrl + "/getUpdates?offset=" + strconv.Itoa(offset))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Запись и обработка полученных данных
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var restResponse mods.TelegramResponse
+	err = json.Unmarshal(body, &restResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return restResponse.Result, nil
+}
+
+//	https://core.telegram.org/bots/api#using-a-local-bot-api-server
+func respond(botUrl string, update mods.Update) error {
+
+	if len(update.Message.Text) <= 0 {
+		return nil
+	}
+
+	switch update.Message.Text {
+	case "/start", "/help":
+		mods.Help(botUrl, update)
+		return nil
+	}
+
+	lenMsg := len(update.Message.Text)
+
+	if update.Message.Text[lenMsg-1] == '?' {
+		mods.Ball8(botUrl, update)
+		return nil
+	}
+
+	mods.SendMsg(botUrl, update, "Немножко не поняла. Не забудь добавить знак вопроса в конце сообщения.")
+	return nil
+
+}
